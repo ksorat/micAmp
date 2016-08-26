684aad8a40019022fe55393667c1ff5bce72a11f
Kareem Sorathia <kareem.sorathia@gmail.com>
2016-08-26 13:21:32 -0400

    Array test

--- test/memTest/main.cpp
+++ test/memTest/main.cpp
@@ -2,77 +2,133 @@
 
 //Allocate contiguous 2D array, x-fer to MIC, do simple calculation and bring back
 
-#define DIMX 1000
-#define DIMY 1000
+#define DIMX 100
+#define DIMY 100
+#define DIMZ 20
+#define DIMV 6
 
+#define ALIGN 64
+
+#define MIC0 mic:0
 #define ALLOC alloc_if(1) free_if(0)
 #define REUSE alloc_if(0) free_if(0)
 #define FREE  alloc_if(0) free_if(1)
+#define RETAIN alloc_if(1) free_if(0)
+
+#define MICFUNC __declspec(target(mic))
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 typedef double Real;
+typedef Real **** RealP4;
 
-Real **Create2Array(int N1, int N2);
-void Kill2Array(Real **ToDie);
+MICFUNC RealP4 Create4Array(int N1, int N2, int N3, int N4);
+MICFUNC void Kill4Array(RealP4 ToDie);
 
 int main() {
-	Real **Data;
-	int i,j;
+	Real ****Data;
+	int i,j,k,n, Ntot;
 	Real s, c, cumsum;
 	//Setup
-	Data = Create2Array(DIMX,DIMY);
-	for (i=0;i<DIMX;i++) {
-		for (j=0;j<DIMY;j++) {
-			Data[i][j] = 1.0*i*j;
+
+	Ntot = DIMX*DIMY*DIMZ*DIMV;
+	printf("Alloc on host\n");
+	Data = Create4Array(DIMV,DIMZ,DIMY,DIMX);
+	printf("Finish alloc on host\n");
+
+	#pragma offload target(MIC0) 
+	printf("Start alloc on MIC\n");
+	{
+		Data = Create4Array(DIMV,DIMZ,DIMY,DIMX);
+	}
+	printf("Finish alloc on MIC\n");
+
+	printf("Initialize data on host\n");
+	for (n=0;n<DIMV;n++) {
+		for (k=0;k<DIMZ;k++) {
+			for (j=0;j<DIMY;j++) {
+				for (i=0;i<DIMX;i++) {
+					Data[n][k][j][i] = 1.0*n*k*j*i;
+				}
+			}
 		}
 	}
 
+	printf("Begin transfer/calculation\n");
 	//Xfer and calculate
-	#pragma offload target(mic:0) inout(Data[0:DIMX][0:DIMY])
+	//#pragma offload target(mic:0) inout(Data[0:DIMX][0:DIMY])
+	//#pragma offload target(mic:0) inout( *Data[0:DIMX] : length(DIMY) )
+	//#pragma offload target(mic:0) inout( Data : length(DIMX*DIMY) ALLOC )	
+	//#pragma offload target(mic:0) inout( Data : length(DIMX*DIMY) ALLOC )	
+	#pragma offload target(MIC0) inout( ****Data : length(Ntot) REUSE )
 	{
-		#pragma omp parallel for private(s,c)
-		for (i=0;i<DIMX;i++) {
-			#pragma omp simd
-			for (j=0;j<DIMY;j++) {
-				s = sin(Data[i][j]);
-				c = cos(Data[i][j]);
-	
-				Data[i][j] = s*s + c*c;
+		#pragma omp parallel for private(s,c) collapse(3)
+		for (n=0;n<DIMV;n++) {
+			for (k=0;k<DIMZ;k++) {
+				for (j=0;j<DIMY;j++) {
+					#pragma omp simd
+					for (i=0;i<DIMX;i++) {
+						s = sin(Data[n][k][j][i]);
+						c = cos(Data[n][k][j][i]);
+						Data[n][k][j][i] = s*s + c*c;
+					}
+				}
 			}
 		}
+
+
 	} //Data region
 
 	//Check sum
 	cumsum = 0;
-	for (i=0;i<DIMX;i++) {
-		for (j=0;j<DIMY;j++) {
-			cumsum += Data[i][j];
+	for (n=0;n<DIMV;n++) {
+		for (k=0;k<DIMZ;k++) {
+			for (j=0;j<DIMY;j++) {
+				for (i=0;i<DIMX;i++) {
+					cumsum += Data[n][k][j][i];
+				}
+			}
 		}
 	}
 
-	printf("Checksum = %f\n", cumsum/(DIMX*DIMY));
-	Kill2Array(Data);
+	printf("Checksum = %f\n", cumsum/(DIMX*DIMY*DIMZ*DIMV));
+	Kill4Array(Data);
 	return 0;
 }
 
-Real **Create2Array(int N1, int N2) {
-	int i;
-	int ind1;
+//Creates array of dimension Data4D[N1][N2][N3][N4]
+RealP4 Create4Array(int N1, int N2, int N3, int N4) {
 
-	Real **Data2D = new Real*[N1];
+	int n,i,j;
+	int ind1, ind2, ind3;
 
-	Data2D[0] = new Real [N1*N2];
-	for (i=0;i<N1;i++) {
-		ind1 = i*N2;
-		Data2D[i] = &Data2D[0][ind1];
+	RealP4 Data4D = (Real ****)_mm_malloc(sizeof(Real****)*N1, ALIGN);
+    Data4D[0] = (Real ***)_mm_malloc(sizeof(Real***)*N1*N2, ALIGN);
+    Data4D[0][0] = (Real **)_mm_malloc(sizeof(Real**)*N1*N2*N3, ALIGN);
+    Data4D[0][0][0] = (Real *)_mm_malloc(sizeof(Real*)*N1*N2*N3*N4, ALIGN);	
+
+	for(n=0;n<N1;n++) {
+		ind1 = n*N2;
+		Data4D[n] = &Data4D[0][ind1];
+		for(i=0;i<N2;i++) {
+			ind2 = n*(N2*N3) + i*N3;
+			Data4D[n][i] = &Data4D[0][0][ind2];
+			for(j=0;j<N3;j++) {
+				ind3 = n*(N2*N3*N4) + i*(N3*N4) + j*N4;
+				Data4D[n][i][j] = &Data4D[0][0][0][ind3];
+
+			}
+		}
 	}
-	return Data2D;
+
+	return Data4D;
 }
 
-void Kill2Array(Real **ToDie) {
-	delete[] ToDie[0];
-	delete[] ToDie;	
+void Kill4Array(RealP4 ToDie) {
+	_mm_free (***ToDie);
+	_mm_free (**ToDie);
+	_mm_free (*ToDie);
+	_mm_free (ToDie);
 }
