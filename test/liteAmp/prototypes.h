bce8572592316c82d04b07895e0d2285aebdc53a
Kareem Sorathia <kareem.sorathia@gmail.com>
2016-09-12 18:20:18 -0400

    MIC function flags

--- test/liteAmp/prototypes.h
+++ test/liteAmp/prototypes.h
@@ -14,11 +14,11 @@ void EnforceBCs(RealP4 State, Grid_S Grid, Model_S Model);
 
 
 //File: amp_array.cpp
-RealP4 Create4Array(int N1, int N2, int N3, int N4);
-RealP4 Map4Array(Real *Start, int N1, int N2, int N3, int N4);
-void Kill4Array(RealP4 ToDie);
-void Wipe4Array(RealP4 Ar, int N1, int N2, int N3, int N4);
-void Copy4Array(RealP4 A, RealP4 B, int N1, int N2, int N3, int N4);
+MICTYPE RealP4 Create4Array(int N1, int N2, int N3, int N4);
+MICTYPE RealP4 Map4Array(Real *Start, int N1, int N2, int N3, int N4);
+MICTYPE void Kill4Array(RealP4 ToDie);
+MICTYPE void Wipe4Array(RealP4 Ar, int N1, int N2, int N3, int N4);
+MICTYPE void Copy4Array(RealP4 A, RealP4 B, int N1, int N2, int N3, int N4);
 
 //File: amp_output.cpp
 void toConsole(RealP4 State, Grid_S Grid, Model_S Model);
@@ -27,10 +27,10 @@ void bswap(void *vdat, int len, int cnt); //Borrowed from athena
 void WriteVar(std::string varname, int varnum, Real ****State, Grid_S Grid, FILE *VTKFile, float *data);
 
 //File integrator.cpp
-void AdvanceFluid(RealP4 State, Grid_S Grid, Model_S Model, Real dt);
-void InitializeIntegrator(Grid_S Grid, Model_S Model);
-void DestroyIntegrator(Grid_S Grid, Model_S Model);
-void FluxUpdate(RealP4 Prim, RealP4 Fx, RealP4 Fy, RealP4 Fz, Real dt, Grid_S Grid);
+MICTYPE void AdvanceFluid(RealP4 State, Grid_S Grid, Model_S Model, Real dt);
+MICTYPE void InitializeIntegrator(Grid_S Grid, Model_S Model);
+MICTYPE void DestroyIntegrator(Grid_S Grid, Model_S Model);
+MICTYPE void FluxUpdate(RealP4 Prim, RealP4 Fx, RealP4 Fy, RealP4 Fz, Real dt, Grid_S Grid);
 
 
 
