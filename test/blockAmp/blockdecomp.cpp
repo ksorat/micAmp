4aef02e05700fc0e1823c3789c1deb01917bf1be
Kareem Sorathia <kareem.sorathia@gmail.com>
2016-09-26 12:00:55 -0400

    Fast push

--- test/blockAmp/blockdecomp.cpp
+++ test/blockAmp/blockdecomp.cpp
@@ -56,7 +56,7 @@ void BlockAdvance(RealP4 State, Grid_S Grid, Model_S Model, Real dt) {
 				#endif
 				{
 					//Advance sub-block
-					AdvanceFluid(Qblk,*myBlock,Model,Grid.dt);
+					//AdvanceFluid(Qblk,*myBlock,Model,Grid.dt);
 				}
 				//Copy advanced sub-block back into advState holder
 				//Avoid ghosts
