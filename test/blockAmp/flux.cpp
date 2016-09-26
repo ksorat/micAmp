90eab5bc795ce7c501de6fdd65c19b43c8a6eeb7
Kareem Sorathia <kareem.sorathia@gmail.com>
2016-09-26 16:48:46 -0400

    Fast push

--- test/blockAmp/flux.cpp
+++ test/blockAmp/flux.cpp
@@ -16,8 +16,8 @@ void Flux_PCM(BlockCC W,BlockIC Fx,BlockIC Fy,BlockIC Fz,Block_S Block,Model_S M
 	// WipeBlockIC(Fz,Block);
 
     LRs2Flux(W,W,Fx,DIR_X,Block);
-    LRs2Flux(W,W,Fy,DIR_Y,Block);
-    LRs2Flux(W,W,Fz,DIR_Z,Block);
+    //LRs2Flux(W,W,Fy,DIR_Y,Block);
+    //LRs2Flux(W,W,Fz,DIR_Z,Block);
 	
 }
 
